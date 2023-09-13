4d5447a7c9230314aa6dab465e229b599060e4ad
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-09-13 19:08:40 +0530

    commit

--- client/src/Components/Menu_Display.jsx
+++ client/src/Components/Menu_Display.jsx
@@ -165,7 +165,7 @@ function Menu_Display() {
               <img
                 src="https://plus.unsplash.com/premium_photo-1677686707480-585883c4f43b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Spaghetti Delle"
-                className="dish-image"
+                // className="dish-image"
               />
               <h2 className="dish-name">Spaghetti Delle</h2>
               <p className="description">
